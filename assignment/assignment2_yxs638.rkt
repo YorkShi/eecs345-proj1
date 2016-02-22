f4c6685902db14704a5b306fab74e3fb4276a10d
ShingJan <yuanj.shi@hotmail.com>
2016-02-22 16:32:35 -0500

    second commit

--- assignment/assignment2_yxs638.rkt
+++ assignment/assignment2_yxs638.rkt
@@ -5,22 +5,19 @@
 (define dotproduct-cps
   (lambda (l1 l2 return)
     (cond
-    ((or (null? l1) (null? l2)) (return 0))
-    (else (+ (* (car l1) (car l2)) (dotproduct-cps (cdr l1) (cdr l2)))))))
+    ((or (null? l1) (null? l2)) (return 1))
+    (else (dotproduct-cps (cdr l1) (cdr l2) (lambda (v) (return (* v(* (car l1) (car l2))))))))))
 
 ; 2. Define a nmethod to compute every round of the squareroot and then use suqareroot to recursively calculate the result
-(define nmethod
-  (lambda (a b)
-    (- a (/ (- (* a a) b) (* a 2)))))
-
-(define squareroot-cps
-  (lambda (a b c)
-    (cond
-      ((null? b) a)
-      (else (squareroot (nmethod a c) (- b 1) c)))))
+(define squareroot
+  (lambda (value it)
+    (if (zero? it)
+        value
+        (- (squareroot value (- it 1)) (/ (- (* (squareroot value ( - it 1))
+        (squareroot value (- it 1))) value) (* 2 (squareroot value (- it 1 ))))))))
 
 ; 3. Use three conditions to remove the first orrurance of each elements in L1 from L2
-(define removesubsequence-cps
+(define removesubsequence
   (lambda (l1 l2 return)
     (cond
       ((null? l1) (return l2))
@@ -29,7 +26,7 @@
 
 ; 4. Replaceall in cps style
 
-(define removeall*-cps
+(define removeall*
   (lambda (a l)
     (cond
       ((null? l) '())
@@ -55,38 +52,15 @@
  (lambda (v m)
    (cond
      ((null? m) '())
-     (else (cons (dotproduct v (car m)) (vectormult v (cdr m)))))))
+     (else (cons (dotproduct-cps v (car m)) (vectormult v (cdr m)))))))
 
 ; 7. matrixmultiply in cps
 
-; 6. use null? and list? to find the first element in list, nested or not
-(define first*
-  (lambda (lis)
-    (cond
-      ((null? lis) '())
-      ((list? (car lis)) (first* (car lis)))
-      (else (car lis)))))
-
-; 7. Use and to deal with the exception when the last element of original list is numnber or list
-(define last*
-  (lambda (lis)
-    (cond
-      ((null? lis) '())
-      ((and (null? (cdr lis)) (list? (car lis))) (last* (car lis)))
-      ((and (null? (cdr lis)) (number? (car lis))) (car lis))
-      (else (last* (cdr lis))))))
-
-; 8. Use the flatten first, then use the inorder? function to solve the problem
-(define numorder*?
- (lambda (list)
-   (cond
-     ((null? list) #t)
-     ((null? (cdr list)) #t)
-     (else (inorder? (flatten list))))))
+; 8. removesubsequence*
 
 
 
-; 10. Write the following function without external helper functions or additional parameters. You do not need to use continuation passing style, but you may use continuations or call-with-current-continuation to assist you. The function suffix takes an atom and a list and returns a list containing all elements that occur after the last occurrence of the atom.
+; 9. Write the following function without external helper functions or additional parameters. You do not need to use continuation passing style, but you may use continuations or call-with-current-continuation to assist you. The function suffix takes an atom and a list and returns a list containing all elements that occur after the last occurrence of the atom.
     (define suffix
       (lambda (a l)
         (letrec ((suffix-cps
