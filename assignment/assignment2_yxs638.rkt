b0bc7dd57ad2f64b7760b8e47982a9a77caa13e6
ShingJan <yuanj.shi@hotmail.com>
2016-02-22 23:39:40 -0500

    third commit

--- assignment/assignment2_yxs638.rkt
+++ assignment/assignment2_yxs638.rkt
@@ -1,38 +1,43 @@
 #lang racket
 
+(define (sum n return)
+  (if (zero? n)
+      return
+      (sum (- n 1) (+ return n))))
 
 ; 1. Use map to calculate the dotproduct, simple and fast
 (define dotproduct-cps
-  (lambda (l1 l2 return)
-    (cond
-    ((or (null? l1) (null? l2)) (return 1))
-    (else (dotproduct-cps (cdr l1) (cdr l2) (lambda (v) (return (* v(* (car l1) (car l2))))))))))
+  (lambda (l1 l2 r)
+    (if (or (null? l1) (null? l2))
+        (r 0)
+        (dotproduct-cps (cdr l1) (cdr l2) (lambda (v) (r (+ (* (car l1) (car l2)) v)))))))
 
 ; 2. Define a nmethod to compute every round of the squareroot and then use suqareroot to recursively calculate the result
 (define squareroot
-  (lambda (value it)
+  (lambda (value it r)
     (if (zero? it)
         value
         (- (squareroot value (- it 1)) (/ (- (* (squareroot value ( - it 1))
         (squareroot value (- it 1))) value) (* 2 (squareroot value (- it 1 ))))))))
 
+; I did this wrong last time and this time I figure out how to do it in non-cps style
+
 ; 3. Use three conditions to remove the first orrurance of each elements in L1 from L2
-(define removesubsequence
+(define removesubsequence-cps
   (lambda (l1 l2 return)
     (cond
       ((null? l1) (return l2))
-      ((eq? (car l1) (car l2)) (removesubsequence (cdr l1) (cdr l2)))
-      (else (cons (car l2) (removesubsequence l1 (cdr l2)))))))
+      ((eq? (car l1) (car l2)) (removesubsequence-cps (cdr l1) (cdr l2) return))
+      (else (removesubsequence-cps l1 (cdr l2) (lambda (v) (return (cons (car l2) v))))))))
 
 ; 4. Replaceall in cps style
-
-(define removeall*
-  (lambda (a l)
+(define replaceall*
+  (lambda (old new l return)
     (cond
-      ((null? l) '())
-      ((list? (car l)) (cons (removeall* a (car l)) (removeall* a (cdr l))))
-      ((eq? (car l) a) (removeall* a (cdr l)))
-      (else (cons (car l) (removeall* a (cdr l)))))))
+      ((null? l) (return '()))
+      ((list? (car l))(cons (replaceall* old new (car l) return)(replaceall* old new (cdr l) return)))
+      ((eq? old (car l)) (cons new (replaceall* old new (cdr l))))
+      (else (cons (car l) (replaceall* old new (cdr l)))))))
 
 ; 5. In reverse* function, use append to keep the format of list
 (define reverse*
