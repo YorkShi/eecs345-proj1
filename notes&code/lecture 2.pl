a20d064308e33e094856ae76b8b6491cd6305c52
ShingJan <yuanj.shi@hotmail.com>
2016-04-20 15:44:34 -0400

    insert before & contains & append

---
+++ notes&code/lecture 2.pl
@@ -0,0 +1,25 @@
+% append two lists
+% myappend (list1, list2, resultlist) is true if and only if resultlist
+% is the result of appending list1 and list2
+
+myappend([],L,i).
+myappend([H | T], L ,[H | R]) :- myappend(T, L, R).
+
+% (define myappend
+%    (lambda (l1 l2)
+%        (if (null? l1)
+%            l2
+%            (cons (car l1) (myappend (cdr l1) l2))))
+
+contains(X, [X | T]).
+contains(X, [H | T]) :- contains(X, T).
+
+% insertbefore a x [w x y z] => [w a x y z]
+
+insertbefore (A, X, [X|_],[A,X|_]).
+insertbefore (A,X,[H|T1],[H|T2]) :- insertbefore(A,XT1,T2).
+
+
+% insertbeforeall a x [w.x.y.x.z.x] => [w,a,x,y,a,x,z,a,x]
+
+% flatten
