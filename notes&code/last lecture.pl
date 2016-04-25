fc74c46660aef781afeb1e6510efb6f1b1e1c2bc
ShingJan <yuanj.shi@hotmail.com>
2016-04-25 15:18:37 -0400

    last lecture note

---
+++ notes&code/last lecture.pl
@@ -0,0 +1,8 @@
+% look up a value in a state: lookup name state
+% remove a binding from a state: remove_binding name state
+% add a binding to a state: add_binding name value state
+% a state is of the form [[x, 5], [y, 10], [z, 3]]
+
+lookup_name_state :- lookup_name_state
+remove_binding :- remove_binding
+add_binding :- add_binding
